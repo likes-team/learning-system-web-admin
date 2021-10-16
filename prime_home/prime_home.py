0a3d54e1c255b6025aa1041703d30eb9007fa765
pythondev0101 <rmontemayor0101@gmail.com>
2021-10-16 22:15:30 +0800

    feat: Add images in home testimonies

--- prime_home/prime_home.py
+++ prime_home/prime_home.py
@@ -6,7 +6,7 @@ class PrimeHomeModule(CoreModule):
     module_name = 'prime_home'
     module_icon = 'fa-tools'
     module_link = 'prime_home.index'
-    module_short_description = "Prime Home"
+    module_short_description = "Home"
     module_long_description = "Prime Home"
     version = '1.0'
     models = []
