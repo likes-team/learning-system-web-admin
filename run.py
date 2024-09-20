810632ca3b4423d7ef57629419f3d99f6c408842
Joy <joylopenaasis@gmail.com>
2024-09-20 13:18:16 +0800

    Use env host and port

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
+host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
+port = int(os.getenv('FLASK_RUN_PORT', 5000))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host=host, port=port)
