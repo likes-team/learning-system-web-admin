b856d45c14f0a26140e629221c7131e5a5fea2d2
Robert Montemayor <rmontemayor0101@gmail.com>
2024-11-14 14:25:36 -0500

    feat: tarp photo to video

--- prime_admin/utils/upload.py
+++ prime_admin/utils/upload.py
@@ -1,6 +1,11 @@
 # function to check file extension
-def allowed_file(filename):
-    allowed = {'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename, file_type="image"):
+    if file_type == "image":
+        allowed = {'png', 'jpg', 'jpeg', 'gif'}
+    elif file_type == "video":
+        allowed = {'mp4', 'mov', 'mkv', 'avi'}
+    else:
+        raise Exception("Likes Error: file_type is not allowed")
 
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in allowed
